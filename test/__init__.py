2bd82a584e51888ce70e2a59f61a6d5b5a2ded66
James Rowe <jnrowe@ukfsn.org>
2008-12-16 09:32:24 +0000

    Merged test infrastructure from py-template.

---
+++ test/__init__.py
@@ -0,0 +1,41 @@
+#
+# vim: set sw=4 sts=4 et tw=80 fileencoding=utf-8:
+#
+"""test - Support code and data for test suite"""
+# Copyright (C) 2007-2008  James Rowe
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+__version__ = "0.1.0"
+__date__ = "2007-11-29"
+__author__ = "James Rowe <jnrowe@ukfsn.org>"
+__copyright__ = "Copyright (C) 2007-2008 James Rowe"
+__license__ = "GNU General Public License Version 3"
+__credits__ = ""
+__history__ = "See Mercurial repository"
+
+from email.utils import parseaddr
+
+__doc__ += """
+
+:version: %s
+:author: `%s <mailto:%s>`__
+:copyright: %s
+:status: WIP
+:license: %s
+""" % ((__version__, ) + parseaddr(__author__) + (__copyright__, __license__))
+
+from test import mock
+
---
+++ test/mock.py
@@ -0,0 +1,85 @@
+#
+# vim: set sw=4 sts=4 et tw=80 fileencoding=utf-8:
+#
+"""mock - Mock objects for doctest code snippets"""
+# Copyright (C) 2007-2008  James Rowe;
+# All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import __builtin__
+import os
+import urllib
+
+from test import grab_net_sources
+SOURCES = dict([(os.path.basename(i), i) for i in grab_net_sources.SOURCES])
+
+BASEDIR = os.path.dirname(__file__)
+
+def isfile(path):
+    """Mock `isfile` to check existence of test files
+
+    :Parameters:
+        path : `str`
+            File to check for existence
+    :rtype: `bool`
+    :return: `True` if file exists, `False` otherwise
+
+    """
+    filename = os.path.basename(path)
+    try:
+        __builtin__.open(os.path.join(BASEDIR, "data", filename))
+    except IOError:
+        return False
+    return True
+
+def _get_test_file(filename):
+    """Open a test data file
+
+    :Parameters:
+        filename : `str`
+            Basename of the test data to open
+    :rtype: `file`
+    :return: Test data
+    :raise IOError: When the file can't be opened for reading
+
+    """
+    if isfile(filename):
+        return __builtin__.open(os.path.join(BASEDIR, "data", filename))
+    else:
+        if filename in SOURCES:
+            raise IOError("`%s' missing.  It can be downloaded from `%s', or "
+                          "alternatively by running the `grab_net_sources' "
+                          "script." % (filename, SOURCES[filename]))
+        else:
+            raise IOError("Can not open `%s'" % filename)
+
+def urlopen(url, data=None, proxies=None):
+    """Mock `urlopen` to open test data files
+
+    :Parameters:
+        url : `str`
+            URL to simulate, basename is used as local file name
+        data : any
+            Ignored, just for compatibility with `urlopen` callers
+        proxies : any
+            Ignored, just for compatibility with `urlopen` callers
+    :rtype: `file`
+    :return: File object from test data directory
+
+    """
+    return _get_test_file(os.path.basename(url))
+urllib.urlopen = urlopen
+
