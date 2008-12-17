172dedc4255195e04df50d25a9f98851e99b7e56
James Rowe <jnrowe@ukfsn.org>
2008-12-17 15:55:03 +0000

    Removed leftover mercurial references.

--- test/__init__.py
+++ test/__init__.py
@@ -24,7 +24,7 @@ __author__ = "James Rowe <jnrowe@ukfsn.org>"
 __copyright__ = "Copyright (C) 2007-2008 James Rowe"
 __license__ = "GNU General Public License Version 3"
 __credits__ = ""
-__history__ = "See Mercurial repository"
+__history__ = "See git repository"
 
 try:
     from email.utils import parseaddr
