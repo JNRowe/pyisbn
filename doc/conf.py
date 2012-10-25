d093203366fb999877ec565eb4cfda8d8e503564
James Rowe <jnrowe@gmail.com>
2012-10-25 14:13:52 +0100

    Use default Sphinx theme.
    With the move to rtfd, the custom theme isn't used.

--- doc/conf.py
+++ doc/conf.py
@@ -15,8 +15,6 @@
 import os
 import sys
 
-import cloud_sptheme as csp
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -103,7 +101,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'cloud'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -113,7 +111,7 @@ html_theme_options = {
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [csp.get_theme_dir(), ]
+html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
