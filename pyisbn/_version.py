a9fb5445217e84fd7982fd6368426e29aea0efe3
James Rowe <jnrowe@gmail.com>
2013-10-15 15:19:19 +0100

    0.6.3 released

--- pyisbn/_version.py
+++ pyisbn/_version.py
@@ -1,9 +1,9 @@
-# This is pyisbn version 0.6.2 (2013-10-12)
+# This is pyisbn version 0.6.3 (2013-10-15)
 # pylint: disable=C0103, C0111, C0121, W0622
 
-dotted = '0.6.2'
-libtool = '6:22'
-hex = 0x000602
-date = '2013-10-12'
-tuple = (0, 6, 2)
-web = 'pyisbn/0.6.2'
+dotted = '0.6.3'
+libtool = '6:23'
+hex = 0x000603
+date = '2013-10-15'
+tuple = (0, 6, 3)
+web = 'pyisbn/0.6.3'
