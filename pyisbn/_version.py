43cc2e3b663fd905ac1c0e1138365282dbb275b2
James Rowe <jnrowe@gmail.com>
2016-01-08 10:59:26 +0000

    Release 1.0.1

--- pyisbn/_version.py
+++ pyisbn/_version.py
@@ -1,10 +1,10 @@
-# This is pyisbn version 1.0.0 (2014-01-31)
+# This is pyisbn version 1.0.1 (2016-01-08)
 # pylint: disable=C0103, C0111, C0121, W0622
 
-dotted = '1.0.0'
-libtool = '10:20'
-hex = 0x010000
-date = '2014-01-31'
-tuple = (1, 0, 0)
-dict = {'micro': 0, 'major': 1, 'minor': 0}
-web = 'pyisbn/1.0.0'
+dotted = '1.0.1'
+libtool = '10:21'
+hex = 0x010001
+date = '2016-01-08'
+tuple = (1, 0, 1)
+dict = {'micro': 0, 'major': 1, 'minor': 1}
+web = 'pyisbn/1.0.1'
