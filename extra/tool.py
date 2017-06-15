4351907f1a0f1a7751c24240c3417b5c51cf2337
James Rowe <jnrowe@gmail.com>
2017-06-15 06:00:36 +0100

    Bump copyright years

--- extra/tool.py
+++ extra/tool.py
@@ -1,21 +1,21 @@
 #! /usr/bin/env python
 # coding=utf-8
 """tool - A simple pyisbn interface for the command line."""
-# Copyright © 2007-2017  James Rowe <jnrowe@gmail.com>
+# Copyright © 2013-2017  James Rowe <jnrowe@gmail.com>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This file is part of pyisbn.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# pyisbn is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# pyisbn is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 #
+# You should have received a copy of the GNU General Public License along with
+# pyisbn.  If not, see <http://www.gnu.org/licenses/>.
 
 # You won't find this pretty… however it was quick to write and it's compatible
 # with 2.4-3.3
--- tests/test_isbn10.py
+++ tests/test_isbn10.py
@@ -1,21 +1,21 @@
 #
 # coding=utf-8
 """test_isbn10 - Test Isbn10 class."""
-# Copyright © 2007-2017  James Rowe <jnrowe@gmail.com>
+# Copyright © 2012-2017  James Rowe <jnrowe@gmail.com>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This file is part of pyisbn.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# pyisbn is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# pyisbn is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 #
+# You should have received a copy of the GNU General Public License along with
+# pyisbn.  If not, see <http://www.gnu.org/licenses/>.
 
 from expecter import expect
 
