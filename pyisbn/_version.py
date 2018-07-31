653cb1798d4f231d552991a1011d6aba1c4de396
James Rowe <jnrowe@gmail.com>
2018-07-31 13:51:03 +0100

    Release 1.2.1

--- pyisbn/_version.py
+++ pyisbn/_version.py
@@ -1,10 +1,10 @@
-# This is pyisbn version 1.2.0 (2018-07-28)
-# pylint: disable=C0103, C0111, C0121, W0622
+# This is pyisbn version 1.2.1 (2018-07-31)
+# pylint: skip-file
 
-dotted = '1.2.0'
-libtool = '12:20'
-hex = 0x010200
-date = '2018-07-28'
-tuple = (1, 2, 0)
-dict = {'major': 1, 'minor': 2, 'micro': 0}
-web = 'pyisbn/1.2.0'
+dotted = '1.2.1'
+libtool = '12:21'
+hex = 0x010201
+date = '2018-07-31'
+tuple = (1, 2, 1)
+dict = {'major': 1, 'minor': 2, 'micro': 1}
+web = 'pyisbn/1.2.1'
