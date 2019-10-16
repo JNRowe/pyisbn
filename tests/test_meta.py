eb8172d7dc663241d07cf30b3d467e9f6ed0ce36
James Rowe <jnrowe@gmail.com>
2019-10-16 06:40:12 +0100

    [QA] Move book data git hook to test
    This catches data file conflicts far more easily.  I guess this betrays
    the fact I use other data locally.

---
+++ tests/test_meta.py
@@ -0,0 +1,29 @@
+#
+"""test_meta - Tests for project maintenance."""
+# Copyright © 2019  James Rowe <jnrowe@gmail.com>
+#
+# This file is part of pyisbn.
+#
+# pyisbn is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# pyisbn is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# pyisbn.  If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0+
+
+import json
+
+
+def test_formatting():
+    with open('tests/books.json') as fp:
+        data = fp.read()
+    content = json.loads(data)
+    dumped = json.dumps(content, indent=4) + "\n"
+    assert data == dumped
