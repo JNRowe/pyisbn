5d3df4f120888dfe698c9afa72ad26540a86afd6
James Rowe <jnrowe@gmail.com>
2019-10-24 22:12:28 +0100

    Release 1.2.3

--- pyisbn/_version.py
+++ pyisbn/_version.py
@@ -1,10 +1,10 @@
-# This is pyisbn version 1.2.2 (2019-10-24)
+# This is pyisbn version 1.2.3 (2019-10-24)
 # pylint: skip-file
 
-dotted = '1.2.2'
-libtool = '12:22'
-hex = 0x010202
+dotted = '1.2.3'
+libtool = '12:23'
+hex = 0x010203
 date = '2019-10-24'
-tuple = (1, 2, 2)
-dict = {'major': 1, 'minor': 2, 'micro': 2}
-web = 'pyisbn/1.2.2'
+tuple = (1, 2, 3)
+dict = {'major': 1, 'minor': 2, 'micro': 3}
+web = 'pyisbn/1.2.3'
