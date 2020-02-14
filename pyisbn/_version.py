e4ffefbc0cb05b48cf1b008b0e2a1a6ab7b6b924
James Rowe <jnrowe@gmail.com>
2020-02-14 07:05:21 +0000

    Release 1.3.1

--- pyisbn/_version.py
+++ pyisbn/_version.py
@@ -1,10 +1,10 @@
-# This is pyisbn version 1.3.0 (2020-02-10)
+# This is pyisbn version 1.3.1 (2020-02-14)
 # pylint: skip-file
 
-dotted = '1.3.0'
-libtool = '13:20'
-hex = 0x010300
-date = '2020-02-10'
-tuple = (1, 3, 0)
-dict = {'major': 1, 'minor': 3, 'micro': 0}
-web = 'pyisbn/1.3.0'
+dotted = '1.3.1'
+libtool = '13:21'
+hex = 0x010301
+date = '2020-02-14'
+tuple = (1, 3, 1)
+dict = {'major': 1, 'minor': 3, 'micro': 1}
+web = 'pyisbn/1.3.1'
